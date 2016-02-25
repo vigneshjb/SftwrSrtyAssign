f6bbba45a53d3bc56e3bd25fe75dfacce491c018
Ubuntu <ubuntu@ip-172-31-10-156.us-west-1.compute.internal>
2016-02-25 05:15:16 +0000

    removing file that persisted

--- assign2/part1/secret.rb
+++
@@ -1,9 +0,0 @@
-
-abort("Incorrect program execution") unless ARGV.length>3
-
-ip_address = ARGV[0]
-interface = ARGV[1]
-type = ARGV[2]
-message = ARGV[3]
-
-puts "ip address = #{ip_address} \ninterface = #{interface} \ntype = #{type} \nmessage = #{message}"
