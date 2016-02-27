0155bf02a01a7a4a0d3b3d31d14aaaab5f5b5383
Ubuntu <ubuntu@ip-172-31-14-185.us-west-1.compute.internal>
2016-02-27 04:21:14 +0000

    part1assign2 done

--- assign2/part1/secret.py
+++ assign2/part1/secret.py
@@ -1,29 +1,48 @@
 #!/usr/bin/python
 
-from scapy.layers.inet import IP, ICMP, UDP
-from scapy.sendrecv import sr
+from scapy.all import *
 import sys
 import random
 
+def getRandNum():
+        return random.randint(1,255)
+
 def ICMP_echo_message(ipaddress, message, interface):
 	print "sending ICMP req to ", ipaddress, " with message ", message, " over ", interface
+	number = getRandNum()
+	for item in message:
+		ip = IP(src="127.0.0.1", dst=ipaddress, proto=1)
+		ip.id = int(hex(ord(item)).split("x")[1] + hex(number).split("x")[1], 16)
+		pack = ip/ICMP()
+		pack.show()
+		sendp(pack, iface = interface)
 	return
 
 def TCP_message(ipaddress, message, interface):
 	print "sending TCP req to ", ipaddress, " with message ", message, " over ", interface
-	return
+	number = getRandNum()
+        for item in message:
+                ip = IP(src="127.0.0.1", dst=ipaddress, proto=6)
+                ip.id = int(hex(ord(item)).split("x")[1] + hex(number).split("x")[1], 16)
+                pack = ip/TCP(sport=1025, dport=80, flags="S")
+		pack.show()
+                sendp(pack, iface = interface)
+        return
 
 def UDP_message(ipaddress, message, interface):
 	print "sending UDP req to ", ipaddress, " with message ", message, " over ", interface
+	number = getRandNum()
+        for item in message:
+                ip = IP(src="127.0.0.1", dst=ipaddress, proto=17)
+                ip.id = int(hex(ord(item)).split("x")[1] + hex(number).split("x")[1], 16)
+                pack = ip/UDP(dport=53)
+		pack.show()
+                sendp(pack, iface = interface)
 	return
 
-def getRandNum():
-	return random.randint(1,255)
-
 if len(sys.argv)!= 5:
-	sys.exit("Incorrect program execution")
+	sys.exit("Incorrect program execution : please use ./secret_sender <ip_address> <interface> <type> <message>")
 
-print 'Number of arguments:', len(sys.argv)
 ip_address = sys.argv[1]
 interface = sys.argv[2]
 type_param = sys.argv[3]
@@ -36,4 +55,3 @@ elif type_param == "1":
 elif type_param == "2":
 	UDP_message(ip_address, message, interface)
 
-print getRandNum()
